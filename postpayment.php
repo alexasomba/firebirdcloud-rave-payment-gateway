6290fee9b4d207bb4ba11fb17a00533526a6d3c7
Alexander Asomba <alexanderasomba@gmail.com>
2019-07-04 11:24:39 +0100

    php 7.2

--- postpayment.php
+++ postpayment.php
@@ -1,9 +1,21 @@
 <?php
 
+ini_set('session.save_handler', 'memcached');
+ini_set('session.save_path', getenv('MEMCACHIER_SERVERS'));
+if(version_compare(phpversion('memcached'), '3', '>=')) {
+    ini_set('memcached.sess_persistent', 1);
+    ini_set('memcached.sess_binary_protocol', 1);
+} else {
+    ini_set('session.save_path', 'PERSISTENT=myapp_session ' . ini_get('session.save_path'));
+    ini_set('memcached.sess_binary', 1);
+}
+ini_set('memcached.sess_sasl_username', getenv('MEMCACHIER_USERNAME'));
+ini_set('memcached.sess_sasl_password', getenv('MEMCACHIER_PASSWORD'));
+
 // Enable MemCachier session support
 session_start();
-$_SESSION['test'] = 42;
-//session_save_path("PERSISTENT=myapp_session ${MEMCACHIER_SERVERS}");//path on your server where you are storing session
+//$_SESSION['test'] = 42;
+//session_save_path(PERSISTENT=myapp_session getenv('MEMCACHIER_SERVERS');//path on your server where you are storing session
 
 //session_save_path("./");//path on your server where you are storing session
 
