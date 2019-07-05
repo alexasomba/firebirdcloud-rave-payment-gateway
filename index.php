bd465fd3d5ef35a749032a940402a0b439c7a080
Alexander Asomba <alexanderasomba@gmail.com>
2019-07-05 11:26:43 +0100

    Updated index.php

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-//Silence is golden
+/**
+ * Silence is golden
+ * 
+ * PHP version 7
+ * 
+ * @category Payment
+ * @package Memcachier
+ * @author Alexander Asomba <alex@asomba.com>
+ * @license MIT License
+ * @link http://alexasomba.com
+ * 
+ * \
